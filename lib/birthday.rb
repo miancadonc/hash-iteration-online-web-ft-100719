505c873de49b26f1005e4358205bf68dc462a213
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 18:14:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,6 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!" if age =< 12
+    puts "Happy Birthday #{name}! You are now #{age} years old!" if age <= 12
   end
 end
