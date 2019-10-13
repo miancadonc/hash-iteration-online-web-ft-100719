6639e078943c4f9d0f8690dfbfb49677a3e9433f
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 18:17:47 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,9 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  passengers.each do |key, value|
+    winner = value if key == :suite_a && value.start_with?("A")
+  end
+  winner
 end
